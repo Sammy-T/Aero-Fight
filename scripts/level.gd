953999d2f6586e26420fa38146ae2bc94c61322e
Sammy-T <cyam9019@aim.com>
2023-03-30 21:27:47 -0400

    Remove unused island removal function

--- scripts/level.gd
+++ scripts/level.gd
@@ -27,9 +27,9 @@ func _ready() -> void:
 	
 	semaphore = Semaphore.new()
 	thread = Thread.new()
-	var _err: Error = thread.start(_process_load_map, Thread.PRIORITY_NORMAL)
+	var _err: Error = thread.start(_load_map, Thread.PRIORITY_NORMAL)
 	
-	_threaded_load_map()
+	_trigger_load_map()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -64,15 +64,15 @@ func _check_section_load(next_direction: Vector2i) -> void:
 	# If the source of the checked tile doesn't exist load the next section
 	if next_load_source == -1:
 		print("load next")
-		_threaded_load_map(next_section)
+		_trigger_load_map(next_section)
 
 
-func _threaded_load_map(offset: Vector2i = Vector2i.ZERO) -> void:
+func _trigger_load_map(offset: Vector2i = Vector2i.ZERO) -> void:
 	map_offset = offset
 	semaphore.post()
 
 
-func _process_load_map() -> void:
+func _load_map() -> void:
 	while true:
 		semaphore.wait()
 		
@@ -90,17 +90,17 @@ func _process_load_map() -> void:
 				var x2_pos: int = (map_offset.x + 1) * MAP_SECTION_SIZE - x - 1
 				var y2_pos: int = (map_offset.y + 1) * MAP_SECTION_SIZE - y - 1
 				
-				_proc_append_or_set_cell(x_pos, y_pos, grass_cells, dirt_cells)
-				_proc_append_or_set_cell(x2_pos, y_pos, grass_cells, dirt_cells)
-				_proc_append_or_set_cell(x_pos, y2_pos, grass_cells, dirt_cells)
-				_proc_append_or_set_cell(x2_pos, y2_pos, grass_cells, dirt_cells)
+				_append_or_set_cell(x_pos, y_pos, grass_cells, dirt_cells)
+				_append_or_set_cell(x2_pos, y_pos, grass_cells, dirt_cells)
+				_append_or_set_cell(x_pos, y2_pos, grass_cells, dirt_cells)
+				_append_or_set_cell(x2_pos, y2_pos, grass_cells, dirt_cells)
 		
 		# Set and connect the terrain tiles
 		tile_map.call_deferred("set_cells_terrain_connect", 0, grass_cells, 0, 0)
 		tile_map.call_deferred("set_cells_terrain_connect", 0, dirt_cells, 0, 2)
 
 
-func _proc_append_or_set_cell(x_pos: int, y_pos: int,\
+func _append_or_set_cell(x_pos: int, y_pos: int,\
 			grass_cells: PackedVector2Array, dirt_cells: PackedVector2Array) -> void:
 	# Create 2x2 clusters of the same noise position sample based on the cell position
 	var noise_pos: Vector2 = (Vector2(x_pos, y_pos) / 2).floor()
@@ -114,11 +114,3 @@ func _proc_append_or_set_cell(x_pos: int, y_pos: int,\
 		dirt_cells.append(cell_coord)
 	else:
 		tile_map.call_deferred("set_cell", 0, cell_coord, 0, TILE_COORD_WATER)
-
-
-func _proc_remove_island_cells(cells: Array[Vector2i], island_tile: Vector2i) -> void:
-	for cell in cells:
-		var tile_coord: Vector2i = tile_map.get_cell_atlas_coords(0, cell)
-		
-		if tile_coord == island_tile:
-			tile_map.call_deferred("set_cell", 0, cell, 0, TILE_COORD_WATER)
