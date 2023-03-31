46f7e4d8efc96e2839d86dd7d7678b734e212aab
Sammy-T <cyam9019@aim.com>
2023-03-30 22:49:59 -0400

    Load 3x3 section area

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 
-const MAP_SECTION_SIZE: int = 32
+const MAP_SECTION_SIZE: int = 8
 const TILE_COORD_WATER: Vector2i = Vector2i(6, 3)
 const TILE_COORD_ISLAND: Vector2i = Vector2i(4, 5)
 const TILE_COORD_ISLAND2: Vector2i = Vector2i(10, 5)
@@ -9,8 +9,10 @@ const TILE_COORD_ISLAND2: Vector2i = Vector2i(10, 5)
 @export var map_noise: Noise
 
 var player: Node2D
-var map_section: Vector2i = Vector2i.ZERO
-var map_offset: Vector2i = Vector2i.ZERO
+var current_section: Vector2i = Vector2i.ONE
+var pending_sections: Array[Vector2i] = []
+var loaded_sections: Array[Vector2i] = []
+var section_offset: Vector2i = Vector2i.ZERO
 var thread: Thread
 var semaphore: Semaphore
 
@@ -19,85 +21,100 @@ var semaphore: Semaphore
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var map_end_pos: Vector2 = tile_map.map_to_local(Vector2i.ONE * MAP_SECTION_SIZE)
+	var section_end_pos: Vector2 = tile_map.map_to_local(Vector2i.ONE * MAP_SECTION_SIZE)
 	
 	# Place the player at the center of the map's load area
 	player = get_tree().get_first_node_in_group("player")
-	player.position = map_end_pos * 0.5
+	player.position = section_end_pos * 0.5 + section_end_pos
 	
 	semaphore = Semaphore.new()
 	thread = Thread.new()
-	var _err: Error = thread.start(_load_map, Thread.PRIORITY_NORMAL)
+	var _err: Error = thread.start(_threaded_load_section, Thread.PRIORITY_NORMAL)
 	
-	_trigger_load_map()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	var section_start_pos: Vector2 = tile_map.map_to_local(map_section * MAP_SECTION_SIZE)
-	var section_end_pos: Vector2 = tile_map.map_to_local(\
-			(map_section + Vector2i.ONE) * MAP_SECTION_SIZE)
+	for x in 3:
+		for y in 3:
+			pending_sections.append(Vector2i(x, y))
 	
-	var load_buffer: int = 50 #500
-	
-	# Check whether the player has entered the load buffer
-	if player.position.x >= section_end_pos.x - load_buffer:
-		_check_section_load(Vector2i.RIGHT)
-	elif player.position.x <= section_start_pos.x + load_buffer:
-		_check_section_load(Vector2i.LEFT)
-	
-	# Update the currently occupied map section
-	if player.position.x >= section_end_pos.x:
-		map_section.x += 1
-		print("section %s" % map_section)
-	elif player.position.x <= section_start_pos.x:
-		map_section.x -= 1
-		print("section %s" % map_section)
-
-
-func _check_section_load(next_direction: Vector2i) -> void:
-	var next_section: Vector2i = map_section + next_direction
-	var next_load_end: Vector2i = next_section * MAP_SECTION_SIZE
-	
-	var next_load_source: int = tile_map.get_cell_source_id(0, next_load_end)
-	
-	# If the source of the checked tile doesn't exist load the next section
-	if next_load_source == -1:
-		print("load next")
-		_trigger_load_map(next_section)
-
-
-func _trigger_load_map(offset: Vector2i = Vector2i.ZERO) -> void:
-	map_offset = offset
 	semaphore.post()
 
 
-func _load_map() -> void:
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(_delta: float) -> void:
+#	var section_start_pos: Vector2 = tile_map.map_to_local(map_section * MAP_SECTION_SIZE)
+#	var section_end_pos: Vector2 = tile_map.map_to_local(\
+#			(map_section + Vector2i.ONE) * MAP_SECTION_SIZE)
+#
+#	var load_buffer: int = 50 #500
+#
+#	# Check whether the player has entered the load buffer
+#	if player.position.x >= section_end_pos.x - load_buffer:
+#		_check_section_load(Vector2i.RIGHT)
+#	elif player.position.x <= section_start_pos.x + load_buffer:
+#		_check_section_load(Vector2i.LEFT)
+#
+#	# Update the currently occupied map section
+#	if player.position.x >= section_end_pos.x:
+#		map_section.x += 1
+#		print("section %s" % map_section)
+#	elif player.position.x <= section_start_pos.x:
+#		map_section.x -= 1
+#		print("section %s" % map_section)
+
+
+#func _check_section_load(next_direction: Vector2i) -> void:
+#	var next_section: Vector2i = map_section + next_direction
+#	var next_load_end: Vector2i = next_section * MAP_SECTION_SIZE
+#
+#	var next_load_source: int = tile_map.get_cell_source_id(0, next_load_end)
+#
+#	# If the source of the checked tile doesn't exist load the next section
+#	if next_load_source == -1:
+#		print("load next")
+#		_trigger_load_map(next_section)
+
+
+#func _trigger_section_load(offset: Vector2i = Vector2i.ZERO) -> void:
+#	section_offset = offset
+#	semaphore.post()
+
+
+func _threaded_load_section() -> void:
 	while true:
 		semaphore.wait()
 		
-		var grass_cells: PackedVector2Array = []
-		var dirt_cells: PackedVector2Array = []
-		
-		var half_section: int = int(MAP_SECTION_SIZE / 2.0)
-		
-		# Use the noise to determine the terrain tiles
-		for x in half_section:
-			for y in half_section:
-				var x_pos: int = x + map_offset.x * MAP_SECTION_SIZE
-				var y_pos: int = y + map_offset.y * MAP_SECTION_SIZE
+		while pending_sections.size() > 0:
+			_load_section()
+
+
+func _load_section() -> void:
+	var section: Vector2i = pending_sections.pop_front()
+	print("loading section ", section)
+	
+	var grass_cells: PackedVector2Array = []
+	var dirt_cells: PackedVector2Array = []
+	
+	var half_section: int = int(MAP_SECTION_SIZE / 2.0)
+	
+	# Use the noise to determine the terrain tiles
+	for x in half_section:
+		for y in half_section:
+			var x_pos: int = x + section.x * MAP_SECTION_SIZE
+			var y_pos: int = y + section.y * MAP_SECTION_SIZE
 				
-				var x2_pos: int = (map_offset.x + 1) * MAP_SECTION_SIZE - x - 1
-				var y2_pos: int = (map_offset.y + 1) * MAP_SECTION_SIZE - y - 1
+			var x2_pos: int = (section.x + 1) * MAP_SECTION_SIZE - x - 1
+			var y2_pos: int = (section.y + 1) * MAP_SECTION_SIZE - y - 1
 				
-				_append_or_set_cell(x_pos, y_pos, grass_cells, dirt_cells)
-				_append_or_set_cell(x2_pos, y_pos, grass_cells, dirt_cells)
-				_append_or_set_cell(x_pos, y2_pos, grass_cells, dirt_cells)
-				_append_or_set_cell(x2_pos, y2_pos, grass_cells, dirt_cells)
-		
-		# Set and connect the terrain tiles
-		tile_map.call_deferred("set_cells_terrain_connect", 0, grass_cells, 0, 0)
-		tile_map.call_deferred("set_cells_terrain_connect", 0, dirt_cells, 0, 2)
+			_append_or_set_cell(x_pos, y_pos, grass_cells, dirt_cells)
+			_append_or_set_cell(x2_pos, y_pos, grass_cells, dirt_cells)
+			_append_or_set_cell(x_pos, y2_pos, grass_cells, dirt_cells)
+			_append_or_set_cell(x2_pos, y2_pos, grass_cells, dirt_cells)
+	
+	# Set and connect the terrain tiles
+	tile_map.call_deferred("set_cells_terrain_connect", 0, grass_cells, 0, 0)
+	tile_map.call_deferred("set_cells_terrain_connect", 0, dirt_cells, 0, 2)
+	
+	print("loaded section ", section)
+	loaded_sections.append(section)
 
 
 func _append_or_set_cell(x_pos: int, y_pos: int,\
